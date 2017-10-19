890a00693123eb37bac15c8137d86121a4d69c03
Shenguito <swggh@iscte-iul.pt>
2017-10-19 13:28:27 +0100

    PuppetMaster created

---
+++ PuppetMaster/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppetMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = Console.ReadLine();
+            while (!text.Equals("exit"))
+            {
+                Console.WriteLine(text);
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
