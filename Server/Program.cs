10097af0925fa6c56b1c8b4d8a66be524133755c
Shenguito <swggh@iscte-iul.pt>
2017-10-20 12:36:24 +0100

    update

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,66 +10,9 @@ namespace pacman
     class Server
     {
 
-        private ArrayList playersAlive;
-        private ArrayList playersDead;
-        private ArrayList coins;
-        private ArrayList monsters;
-        private ArrayList walls;
-        private ArrayList inputs;
-        private int MSECROUND = 10; //game speed
-
-        public Server()
+        static void Main(string[] args)
         {
-
-        }
-
-        public void run()
-        {
-            while (true)
-            {
-                System.Threading.Thread.Sleep(MSECROUND);
-                UpdateBoard();
-                ResetInputsReceived();
-            }
-        }
-
-        public void receiveInputs()
-        {
-            //TODO receber inputs dos clientes e atualizar parametros Inputs
-        }
-
-        private void ResetInputsReceived()
-        {
-            inputs = new ArrayList();
-        }
-
-        private void UpdateBoard()
-        {
-            Move();
-            CheckDeadPlayers();
-            CheckCoinsRetrieved();
-            CheckGameEnd();
-
-        }
-
-        private void CheckGameEnd()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CheckCoinsRetrieved()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CheckDeadPlayers()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Move()
-        {
-            throw new NotImplementedException();
+            
         }
     }
 }
---
+++ Server/Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+
+        private ArrayList playersAlive;
+        private ArrayList playersDead;
+        private ArrayList coins;
+        private ArrayList monsters;
+        private ArrayList walls;
+        private ArrayList inputs;
+        private int MSECROUND = 10; //game speed [communication refresh time]
+
+        public Server()
+        {
+
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(MSECROUND);
+                UpdateBoard();
+                ResetInputsReceived();
+            }
+        }
+
+        public void receiveInputs()
+        {
+            //TODO receber inputs dos clientes e atualizar parametros Inputs
+        }
+
+        private void ResetInputsReceived()
+        {
+            inputs = new ArrayList();
+        }
+
+        private void UpdateBoard()
+        {
+            Move();
+            CheckDeadPlayers();
+            CheckCoinsRetrieved();
+            CheckGameEnd();
+
+        }
+
+        private void CheckGameEnd()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckCoinsRetrieved()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckDeadPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
